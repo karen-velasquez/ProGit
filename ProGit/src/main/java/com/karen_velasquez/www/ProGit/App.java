11959c7dc231c0fe45bee0fd01df9ede480ed800
karen <karen@karen>
2019-09-12 12:32:16 -0400

    first commit

---
+++ ProGit/src/main/java/com/karen_velasquez/www/ProGit/App.java
@@ -0,0 +1,13 @@
+package com.karen_velasquez.www.ProGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
