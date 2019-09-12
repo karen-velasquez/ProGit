4107e40a96ff3704adb6bb5d204e6cd34cd86578
karen <karen@karen>
2019-09-12 13:26:26 -0400

    Creación de la clase Application

---
+++ ProGit/src/main/java/com/karen_velasquez/www/ProGit/MinibusesApplication.java
@@ -0,0 +1,12 @@
+package com.karen_velasquez.www.ProGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibusesApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(MinibusesApplication.class, args);
+  }
+}
