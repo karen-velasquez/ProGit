a98fd16bbca4becff75c66bf76145ec78916559e
karen <karen@karen>
2019-09-12 12:50:42 -0400

    Creación Clase Minibus

---
+++ ProGit/src/test/java/com/karen_velasquez/www/ProGit/Minibus.java
@@ -0,0 +1,22 @@
+package com.karen_velasquez.www.ProGit;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+	@Entity
+	class Minibus{
+		private @Id @GeneratedValue Long id;
+		private int numero;
+		private String p_partida;
+		 
+		Minibus(){}
+			Minibus(int numero, String p_partida){
+				this.numero=numero;
+				this.p_partida=p_partida;
+		}
+
+}
